48fdec68ccde7147c4517617846f38405f200e8d
Satvik Tiwari <satvikk.tiwariii@gmail.com>
2025-09-06 21:50:16 +0530

    nav updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 export default function Home() {
   return (
     <div className="bg-[#f1e9da] min-h-screen">
       <Navbar></Navbar>
+      <div className="h-50"></div>
+      <Footer></Footer>
     </div>
   );
 }
